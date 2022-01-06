0a2dea3141f9f955a8b3e7a27c532b69532d2b3b
Chirag Bhansali <62190209+bhansalichirag@users.noreply.github.com>
2022-01-05 20:32:45 -0500

    Time: 22 ms (5.16%), Space: 41.4 MB (16.67%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] arr = new int[1001];
+        Arrays.sort(trips,(a,b)->a[1]-b[1]);
+        for(int i=0;i<trips.length;i++)
+        {
+            for(int j=trips[i][1];j<trips[i][2];j++)
+            {
+                arr[j]+=trips[i][0];
+                if(arr[j]>capacity)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
