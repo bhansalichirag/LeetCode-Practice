4606b4267fde3ea1fd5eac6f7da2a75166ea2861
Chirag Bhansali <62190209+bhansalichirag@users.noreply.github.com>
2022-03-05 15:15:52 -0500

    Time: 11 ms (53.67%), Space: 45.6 MB (18.60%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,33 +1,22 @@
 class Solution {
-    List<Integer> end_time = new ArrayList<>();
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
-        
-        for(int[] arr : intervals)
+        List<Integer> li = new ArrayList<>();
+        for(int[] interval : intervals)
         {
-            //System.out.println(Arrays.toString(arr));
-            int end = arr[1];
             int flag = 1;
-            int room = 0;
-            for(int i=0;i<end_time.size();i++)
+            for(int i=0;i<li.size();i++)
             {
-                if(end_time.get(i)<=arr[0])
+                if(li.get(i)<=interval[0])
                 {
                     flag = 0;
-                    room = i;
+                    li.set(i,interval[1]);
                     break;
                 }
             }
-            if(flag==0)
-            {
-                end_time.set(room,end);
-            }
-            else if(flag==1)
-            {
-                end_time.add(end);
-            }
-           // System.out.println("ancdef"+end_time);
+            if(flag==1)
+                li.add(interval[1]);
         }
-        return end_time.size();
+        return li.size();
     }
 }
